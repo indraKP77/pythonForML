05ba3753b9532ec665d079457f3270dec7e0dff2
Indra Kumar <indravip3@gmail.com>
2026-09-14 12:41:08 +0530

    fourth commit

--- walrusOperator.py
+++ walrusOperator.py
@@ -3,3 +3,23 @@ a = 'hello indra'
 
 if ((n := len(a))>10):
     print(f"too long {n} elements")
+
+
+#Scope - what variables do I have access to?
+
+#1 - start with local
+#2 - Parent local?
+#3 - Global
+#4 - built in python functions.
+
+total = 0
+
+def count():
+    global total
+    total += 1
+    return total
+
+count()
+count()
+count()
+print(count())
